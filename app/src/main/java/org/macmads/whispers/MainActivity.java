5986589ba0d102348a4bb540223ad34b83899809
Abdullah Cheema <abdullahcheema.macmads@gmail.com>
2019-09-21 14:17:15 +0500

    adding to gitab for the first time

---
+++ app/src/main/java/org/macmads/whispers/MainActivity.java
@@ -0,0 +1,282 @@
+package org.macmads.whispers;
+
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.net.wifi.p2p.WifiP2pManager.DnsSdTxtRecordListener;
+import android.net.wifi.p2p.WifiP2pManager.DnsSdServiceResponseListener;
+
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest;
+import android.os.Build;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.macmads.whispers.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION = 1001;
+
+    WifiP2pManager manager;
+    Channel channel;
+    BroadcastReceiver receiver;
+    IntentFilter intentFilter;
+    final HashMap<String, String> dnsRecords = new HashMap<String, String>();
+
+
+    List devicesList = new ArrayList<WifiP2pDevice>();
+    ArrayAdapter deviceListAdapter;
+
+    ListView devicesListView;
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION:
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "Coarse location permission is not granted!",
+                            Toast.LENGTH_SHORT).show();
+                    System.out.println("Coarse location permission is not granted!");
+                    finish();
+                }
+                break;
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        devicesListView = (ListView) findViewById(R.id.wifiDevicesList);
+        deviceListAdapter = new ArrayAdapter<String>(this, R.layout.activity_devices_list_item, devicesList);
+        devicesListView.setAdapter(deviceListAdapter);
+
+
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+        receiver = new WiFiDirectBroadcastReceiver(manager, channel, this);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MainActivity.PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION);
+            // After this point you wait for callback in
+            // onRequestPermissionsResult(int, String[], int[]) overridden method
+        }
+        LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        System.out.println("location: ");
+        System.out.println(location);
+
+        DnsSdTxtRecordListener txtListener = new DnsSdTxtRecordListener() {
+            @Override
+            /* Callback includes:
+             * fullDomain: full domain name: e.g "printer._ipp._tcp.local."
+             * record: TXT record dta as a map of key/value pairs.
+             * device: The device running the advertised service.
+             */
+
+            public void onDnsSdTxtRecordAvailable(
+                    String fullDomain, Map record, WifiP2pDevice device) {
+//                Log.d(TAG, "DnsSdTxtRecord available -" + record.toString());
+                Toast.makeText(MainActivity.this, "DnsSdTxtRecord available -" + record.toString(),
+                        Toast.LENGTH_LONG).show();
+                dnsRecords.put(device.deviceAddress, "wifi groups: "+((String) record.get("wifi_ssid")));
+                System.out.println("dns records recieved");
+
+            }
+        };
+
+
+        DnsSdServiceResponseListener servListener = new DnsSdServiceResponseListener() {
+            @Override
+            public void onDnsSdServiceAvailable(String instanceName, String registrationType,
+                                                WifiP2pDevice resourceType) {
+
+                System.out.println("dns service available");
+                resourceType.deviceName = dnsRecords
+                        .containsKey(resourceType.deviceAddress) ? dnsRecords
+                        .get(resourceType.deviceAddress) : resourceType.deviceName;
+                // Add to the custom adapter defined specifically for showing
+                // wifi devices.
+//                WiFiDirectServicesList fragment = (WiFiDirectServicesList) getFragmentManager()
+//                        .findFragmentById(R.id.frag_peerlist);
+//                WiFiDevicesAdapter adapter = ((WiFiDevicesAdapter) fragment
+//                        .getListAdapter());
+//
+//                adapter.add(resourceType);
+//                adapter.notifyDataSetChanged();
+//                deviceListAdapter.clear();
+                deviceListAdapter.add(resourceType);
+                deviceListAdapter.notifyDataSetChanged();
+
+
+            }
+        };
+
+        manager.setDnsSdResponseListeners(channel, servListener, txtListener);
+
+        WifiP2pDnsSdServiceRequest serviceRequest = WifiP2pDnsSdServiceRequest.newInstance();
+        manager.addServiceRequest(channel,
+                serviceRequest,
+                new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        // Success!
+                        System.out.println("service request added successfully");
+                    }
+
+                    @Override
+                    public void onFailure(int code) {
+                        // Command failed.  Check for P2P_UNSUPPORTED, ERROR, or BUSY
+                    }
+                });
+
+        manager.discoverServices(channel, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                // Success!
+                System.out.println("service discovery started successfully");
+            }
+
+            @Override
+            public void onFailure(int code) {
+                // Command failed.  Check for P2P_UNSUPPORTED, ERROR, or BUSY
+
+            }
+        });
+
+
+        devicesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                System.out.println(adapterView.getAdapter().getItem(i));
+
+                WifiP2pDevice device = (WifiP2pDevice) adapterView.getAdapter().getItem(i);
+                WifiP2pConfig config = new WifiP2pConfig();
+                config.deviceAddress = device.deviceAddress;
+                config.wps.setup = WpsInfo.PBC;
+
+                manager.connect(channel, config, new WifiP2pManager.ActionListener() {
+
+                    @Override
+                    public void onSuccess() {
+                        // WiFiDirectBroadcastReceiver notifies us. Ignore for now.
+                        Toast.makeText(MainActivity.this, "Connection Successful.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(int reason) {
+                        Toast.makeText(MainActivity.this, "Connect failed. Retry.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+    }
+
+    WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
+        @Override
+        public void onPeersAvailable(WifiP2pDeviceList peers) {
+            deviceListAdapter.clear();
+            System.out.println("peers: ");
+            System.out.println(peers.getDeviceList());
+//            for (WifiP2pDevice peer:peers.getDeviceList()){
+//                deviceListAdapter.add(peer.deviceName);
+//            }
+            deviceListAdapter.addAll(peers.getDeviceList());
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, intentFilter);
+    }
+
+    /* unregister the broadcast receiver */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+
+    public void btnDiscoverPeersOnClick(View view) {
+//        manager.requestDiscoveryState(channel,discoveryStateListener);
+        deviceListAdapter.clear();
+        System.out.println("button pressed");
+        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(MainActivity.this, "peers discovery started successfully",
+                        Toast.LENGTH_SHORT).show();
+                System.out.println("peers discovery started successfully");
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+                Toast.makeText(MainActivity.this, "peers discovery failed",
+                        Toast.LENGTH_SHORT).show();
+                System.out.println("peers discovery failed");
+            }
+        });
+    }
+
+    public void btnHandler(View view) {
+        manager.createGroup(channel, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                // WiFiDirectBroadcastReceiver notifies us. Ignore for now.
+                Toast.makeText(MainActivity.this, "Group Creation Successful.",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                Toast.makeText(MainActivity.this, "Group Creation failed. Retry.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/org/macmads/whispers/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,108 @@
+package org.macmads.whispers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pGroup;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+
+
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
+    private MainActivity mActivity;
+
+
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, Channel channel,
+                                       MainActivity activity) {
+        super();
+        this.mManager = manager;
+        this.mChannel = channel;
+        this.mActivity = activity;
+    }
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Check to see if Wi-Fi is enabled and notify appropriate activity
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                // Wifi P2P is enabled
+                System.out.println("wifi p2p is enabled");
+            } else {
+                // Wi-Fi P2P is not enable
+                System.out.println("wifi p2p is not enabled");
+            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+            if (mManager != null) {
+                mManager.requestPeers(mChannel, mActivity.peerListListener);
+            }
+
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Respond to new connection or disconnections
+            System.out.println("connection state changed");
+            mManager.requestGroupInfo(mChannel, new WifiP2pManager.GroupInfoListener() {
+                @Override
+                public void onGroupInfoAvailable(WifiP2pGroup wifiP2pGroup) {
+                    if (wifiP2pGroup != null) {
+                        String ssid = wifiP2pGroup.getNetworkName();
+                        String password = wifiP2pGroup.getPassphrase();
+                        Toast.makeText(context, "group available", Toast.LENGTH_SHORT);
+                        System.out.println("ssid: ");
+                        System.out.println(ssid);
+                        System.out.println("password: ");
+                        System.out.println(password);
+                    }
+
+                }
+            });
+
+
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+            System.out.println("device state changed");
+            mManager.requestGroupInfo(mChannel, new WifiP2pManager.GroupInfoListener() {
+                @Override
+                public void onGroupInfoAvailable(WifiP2pGroup wifiP2pGroup) {
+                    if (wifiP2pGroup != null) {
+                        String ssid = wifiP2pGroup.getNetworkName();
+                        String password = wifiP2pGroup.getPassphrase();
+                        Toast.makeText(context, "group available", Toast.LENGTH_SHORT);
+                        System.out.println("ssid: ");
+                        System.out.println(ssid);
+                        System.out.println("password: ");
+                        System.out.println(password);
+                        Map record = new HashMap();
+                        record.put("wifi_ssid",ssid);
+                        record.put("wifi_password",password);
+                        WifiP2pDnsSdServiceInfo serviceInfo = WifiP2pDnsSdServiceInfo.newInstance("_test", "_presence._tcp", record);
+                        mManager.addLocalService(mChannel, serviceInfo, new WifiP2pManager.ActionListener() {
+                            @Override
+                            public void onSuccess() {
+                                System.out.println("service added successfully");
+                            }
+
+                            @Override
+                            public void onFailure(int i) {
+
+                            }
+                        });
+
+
+                    }
+
+                }
+            });
+        }
+    }
+}
