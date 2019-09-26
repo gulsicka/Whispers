ebb1b091ac08d0b73e4d5a24bba936b4a3e9a7fd
Gulsicka <Gulsicka13@gmail.com>
2019-09-26 11:12:48 +0500

    Update RowModel.java

--- app/src/main/java/org/macmads/whispers/RowModel.java
+++ app/src/main/java/org/macmads/whispers/RowModel.java
@@ -2,12 +2,15 @@ package org.macmads.whispers;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.wifi.p2p.WifiP2pDevice;
 
 public class RowModel {
     String name;
-    public RowModel(String name)
+    WifiP2pDevice device;
+    public RowModel(String name, WifiP2pDevice device)
     {
-        this.name=name;
+        this.name = name;
+        this.device = device;
     }
 
     public String getName() {
